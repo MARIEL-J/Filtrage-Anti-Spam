b3b91017de778eb9f969a08be518251075baff9d
MARIEL-J <126705749+MARIEL-J@users.noreply.github.com>
2025-01-06 10:06:17 +0100

    app.py

--- app.py
+++ app.py
@@ -16,55 +16,59 @@ import string
 
 ps = PorterStemmer()
 
+import logging
+
+# Configurer le logging
+logging.basicConfig(level=logging.DEBUG)
+
+ps = PorterStemmer()
+
 def transform_text(text):
-    """
-    Applique plusieurs étapes de prétraitement sur le texte fourni :
-   
-    - Détection de la langue et traduction en anglais si nécessaire.
-    - Conversion en minuscules.
-    - Tokenisation (division du texte en mots).
-    - Suppression des mots non alphanumériques, stopwords et ponctuation.
-    - Application du stemming.
-
-    Args:
-    - text (str): Le texte à transformer.
-
-    Returns:
-    - str: Le texte transformé.
-    """
     try:
+        logging.debug("Début de la transformation du texte.")
+        
         # Initialiser le traducteur
         translator = Translator()
         
         # Détection de la langue
         detected_lang = translator.detect(text).lang
+        logging.debug(f"Langue détectée : {detected_lang}")
         
         # Traduction en anglais si la langue détectée n'est pas l'anglais
         if detected_lang != 'en':
             text = translator.translate(text, src=detected_lang, dest='en').text
+            logging.debug(f"Texte traduit : {text}")
         
-        # 1. Conversion du texte en minuscules
+        # Conversion du texte en minuscules
         text = text.lower()
+        logging.debug(f"Texte en minuscules : {text}")
         
-        # 2. Tokenisation du texte
+        # Tokenisation du texte
         text = nltk.word_tokenize(text)
+        logging.debug(f"Texte tokenisé : {text}")
         
-        # 3. Suppression des mots non alphanumériques
+        # Suppression des mots non alphanumériques
         text = [word for word in text if word.isalnum()]
+        logging.debug(f"Texte après suppression des mots non alphanumériques : {text}")
         
-        # 4. Suppression des stopwords et de la ponctuation
+        # Suppression des stopwords et de la ponctuation
         stop_words = set(stopwords.words('english'))
         text = [word for word in text if word not in stop_words and word not in string.punctuation]
+        logging.debug(f"Texte après suppression des stopwords et de la ponctuation : {text}")
         
-        # 5. Application du stemming
+        # Application du stemming
         text = [ps.stem(word) for word in text]
+        logging.debug(f"Texte après stemming : {text}")
         
-        # 6. Retourner le texte transformé
-        return " ".join(text)
+        # Retourner le texte transformé
+        transformed_text = " ".join(text)
+        logging.debug(f"Texte transformé final : {transformed_text}")
+        return transformed_text
     
     except Exception as e:
-        print(f"Erreur lors du traitement : {e}")
-        return None
+        logging.error(f"Erreur lors du traitement : {e}")
+        return ""
+
 
 
 st.set_page_config(
